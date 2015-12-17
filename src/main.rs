6b095d67e2664f51b537e5195fc559d87c9f8dd2
Tommaso Checchi <tommaso.checchi1@gmail.com>
2015-12-17 23:55:34 +0100

    track more things, use f64 and interface for round planets!

--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,61 @@ extern crate rand;
 extern crate gnuplot;
 extern crate cgmath;
 
-use std::cmp;
-use std::cmp::Ordering;
 use gnuplot::*;
-use std::f32;
+use std::f64;
 use std::collections::VecDeque;
 use rand::*;
 use cgmath::{Vector, Vector2, EuclideanVector};
 
-const COEFFICIENTS:usize = 4;
-const SIM_STEP:f32 = 0.1;
+const COEFFICIENTS:usize = 2;
+const SIM_STEP:f64 = 0.1;
 const G:f64 = 6.67384E-11;
 const PLANET_MASS:f64 = 5.2915793E22;
 const PLANET_RADIUS:f64 = 600000.;
+const PLANET_CENTER:Vector2<f64> = Vector2{x: 0., y: PLANET_RADIUS};
 
-fn gravity_at(h: f32) -> f32 {
-	((G * PLANET_MASS) / (PLANET_RADIUS + h as f64).powf(2.)) as f32
+fn gravity_at(pos: Vector2<f64>) -> Vector2<f64> {
+	Vector2{
+		x: 0.,
+		y: -((G * PLANET_MASS) / (PLANET_RADIUS + pos.y).powf(2.))
+	}
 }
 
-fn lerp(src: f32, dst: f32, alpha: f32) -> f32 {
+fn altitude(pos: Vector2<f64>) -> f64 {
+	pos.y
+}
+
+fn lerp(src: f64, dst: f64, alpha: f64) -> f64 {
 	src + (dst - src) * alpha
 }
 
-fn atmospheric_pressure(h: f32) -> f32 {
+fn atmospheric_pressure(pos: Vector2<f64>) -> f64 {
 	let H = 70000.;
-	if h >= H {
+	if pos.y >= H {
 		0.
 	}
 	else {
-		f32::consts::E.powf(-h/70000.)
+		f64::consts::E.powf(-pos.y/70000.)
 	}
 }
 
 struct Vessel {
-    max_thrust: f32,
-    max_burn: f32,
-    dry_mass: f32,
-    fuel_mass: f32,
-    cross_section: f32,
-    starting_height: f32,
+    max_thrust: f64,
+    max_burn: f64,
+    dry_mass: f64,
+    fuel_mass: f64,
+    cross_section: f64,
+    starting_height: f64,
 }
 
 impl Vessel {
-	fn mass(&self) -> f32 {
+	fn mass(&self) -> f64 {
 		self.dry_mass + self.fuel_mass
 	}
 
-	fn find_throttle_for_twr(&self, twr: f32, h:f32) -> f32 {
+	fn find_throttle_for_twr(&self, twr: f64, pos: Vector2<f64>) -> f64 {
 		assert!(twr > 0.);
-		let t = (twr * self.mass() * gravity_at(h)) / self.max_thrust;
+		let t = (twr * self.mass() * gravity_at(pos).length()) / self.max_thrust;
 		assert!(t <= 1.);
 		t
 	}
@@ -58,9 +64,9 @@ impl Vessel {
 
 #[derive(Clone,Debug)]
 struct Control {
-    coeff: [f32; COEFFICIENTS],
-    twr: f32,
-    fitness: Option<f32>
+    coeff: [f64; COEFFICIENTS],
+    twr: f64,
+    fitness: Option<f64>
 }
 
 impl Control {
@@ -80,7 +86,7 @@ impl Control {
 		}
 	}
 
-    fn lerp(a: &Self, b:&Self, s: f32) -> Self {
+    fn lerp(a: &Self, b:&Self, s: f64) -> Self {
     	let mut res = Control::zero();
 
     	for i in 0..res.coeff.len() {
@@ -113,11 +119,11 @@ impl Control {
     	)
     }
 
-    fn calc_angle(&self, t: f32) -> f32{
-    	let mut control_angle = f32::consts::FRAC_PI_2; //start vertical
+    fn calc_angle(&self, t: f64) -> f64{
+    	let mut control_angle = f64::consts::FRAC_PI_2; //start vertical
 
     	for i in 0..self.coeff.len() {	
-    		control_angle += self.coeff[i] * t.powf(i as f32 + 1.);
+    		control_angle += self.coeff[i] * t.powf(i as f64 + 1.);
     	}
     	control_angle
     }
@@ -133,50 +139,65 @@ impl Control {
     }
 
     fn calc_fitness(&mut self, vessel: &Vessel) {
-    	let target_y = 71000.;
+    	let target_alt = 71000.;
 
     	let endpoint = self.calc_endpoint(vessel);
 
-    	let distance_from_target = (target_y - endpoint.pos.y).abs();
+    	let distance_from_target = (target_alt - endpoint.altitude()).abs();
 
-    	let f = distance_from_target - endpoint.vel.x;
+    	let f = distance_from_target - endpoint.tangent_speed();
 
-    	self.fitness = if f.is_normal() && endpoint.pos.y > 10. {
+    	self.fitness = if f.is_normal() && endpoint.altitude() > 10. {
     		Some(f)
     	}
     	else {
-    		Some(f32::MAX)
+    		Some(f64::MAX)
     	}
     }
 }
 
 #[derive(Debug)]
 struct Trajectory {
-	pos: Vector2<f32>,
-	vel: Vector2<f32>,
-	fuel_mass: f32,
-	t: f32,
-	throttle: f32
+	pos: Vector2<f64>,
+	vel: Vector2<f64>,
+	fuel_mass: f64,
+	t: f64,
+	throttle: f64,
+	control_angle:f64,
 }
 
 impl Trajectory {
 	fn new(vessel: &Vessel, control: &Control) -> Self {
+		let start_pos = Vector2::new(0.,vessel.starting_height);
 		Trajectory {
-			pos: Vector2::new(0.,vessel.starting_height),
+			pos: start_pos,
 			vel: Vector2::new(0.,0.),
 			t: 0.,
 			fuel_mass: vessel.fuel_mass,
-			throttle: vessel.find_throttle_for_twr(control.twr, vessel.starting_height),
+			throttle: vessel.find_throttle_for_twr(control.twr, start_pos),
+			control_angle: f64::consts::FRAC_PI_2,
 		}
 	}
 
-	fn step(&mut self, control: &Control, vessel: &Vessel, dt: f32) -> bool {
+	fn altitude(&self) -> f64 {
+		altitude(self.pos)
+	}
+
+	fn vertical_speed(&self) -> f64 {
+		self.vel.y
+	}
+
+	fn tangent_speed(&self) -> f64 {
+		self.vel.x
+	}
+
+	fn step(&mut self, control: &Control, vessel: &Vessel, dt: f64) -> bool {
 		
 		self.t += dt;
 
 		let drag = if self.vel.length2() > 0. && false /*HACK*/ {
 			let V = self.vel.length();
-			let p = atmospheric_pressure(self.pos.y);
+			let p = atmospheric_pressure(self.pos);
 
 			let d = p * V * V * vessel.cross_section * 0.00001;
 
@@ -191,32 +212,34 @@ impl Trajectory {
 
 			let mass = self.fuel_mass + vessel.dry_mass;
 
-			let control_angle = control.calc_angle(self.t);
+			self.control_angle = control.calc_angle(self.t);
 
 			let mut accel = (self.throttle * vessel.max_thrust) / mass;
 
 			Vector2::new(
-				accel * control_angle.cos(),
-				accel * control_angle.sin(),
+				accel * self.control_angle.cos(),
+				accel * self.control_angle.sin(),
 			)
 		} 
 		else {
 			Vector2::new(0.,0.)
 		};
 
-		self.vel.x += (thrust.x + drag.x) * dt;
-		self.vel.y += (thrust.y + drag.y - gravity_at(self.pos.y)) * dt;
+		let g = gravity_at(self.pos);
+
+		self.vel.x += (thrust.x + drag.x + g.x) * dt;
+		self.vel.y += (thrust.y + drag.y + g.y) * dt;
 
 		self.pos.x += self.vel.x * dt;
 		self.pos.y += self.vel.y * dt;
 
-		self.vel.y > 0.
+		self.vertical_speed() > 0.
 	}
 }
 
 struct Plot {
 	figure: Figure,
-	trajectories: VecDeque<(Vec<f32>,Vec<f32>)>,
+	trajectories: VecDeque<(Vec<f32>,Vec<f32>,Vec<f32>,Vec<f32>)>,
 }
 
 impl Plot {
@@ -229,21 +252,20 @@ impl Plot {
 
     fn add_trajectory(&mut self, vessel: &Vessel, control: &Control) {
 
+    	let RADDEG = 180. / f64::consts::PI;
+
 		let mut trajectory = Trajectory::new(&vessel, &control);
 
 		let mut x_points:Vec<f32> = vec![];
 		let mut y_points:Vec<f32> = vec![];
+		let mut a_points:Vec<f32> = vec![];
+		let mut f_points:Vec<f32> = vec![];
 
-	   	loop {
-
-	   		trajectory.step(&control, &vessel, SIM_STEP);
-
-	   		if trajectory.pos.y < 0. {
-	   			break;
-	   		}
-
-		    x_points.push(trajectory.pos.x);
-		    y_points.push(trajectory.pos.y);
+	   	while trajectory.step(&control, &vessel, SIM_STEP) {
+		    x_points.push(trajectory.pos.x as f32);
+		    y_points.push(trajectory.pos.y as f32);
+		    a_points.push((trajectory.control_angle * RADDEG * 1000.) as f32);
+			f_points.push(trajectory.fuel_mass as f32 * 10.);
 		}
 
 		if x_points.len() > 2 {
@@ -253,7 +275,9 @@ impl Plot {
 
 			self.trajectories.push_back((
 				x_points,
-				y_points
+				y_points,
+				a_points,
+				f_points,
 			));
 		}
     }
@@ -276,6 +300,24 @@ impl Plot {
 					&[
 						Caption("Best ascent"),
 						Color("black")
+					]
+				);
+
+				plot.lines(
+					&t.0,
+					&t.2,
+					&[
+						Caption("Angles"),
+						Color("red")
+					]
+				);
+
+				plot.lines(
+					&t.0,
+					&t.3,
+					&[
+						Caption("Fuel"),
+						Color("blue")
 					]
 				);
 			}
@@ -311,7 +353,7 @@ fn main() {
 
 	let mut solutions:Vec<Control> = vec![Control::zero(); size];
 
-	let mut best_fitness = f32::MAX;
+	let mut best_fitness = f64::MAX;
 	loop {
 		sort_solutions(&mut solutions, &vessel);
 
